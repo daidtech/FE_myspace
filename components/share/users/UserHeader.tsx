a90bbd5015fcb5120cf5da70b2abeab6bb1edbae
daidv <dvdai63@gmail.com>
2023-12-21 07:32:48 +0700

    update header

--- components/share/users/UserHeader.tsx
+++ components/share/users/UserHeader.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Container, Row, Nav, Navbar, NavDropdown, Col, Stack } from 'react-bootstrap';
-
+import styles from 'styles/UserHeader.module.css'
 function UserHeader() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
@@ -10,27 +10,33 @@ function UserHeader() {
             <Navbar.Brand href="#home">DaiDV</Navbar.Brand>
           </Col>
           <Col xs={10} className=''>
-            <Stack direction="horizontal" gap={3}>
+            <div className={'toggle-header ' + styles.toggleHeader}>
               <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="justify-content-end w-100">
-                  <Nav.Link href="#home">Home</Nav.Link>
-                  <Nav.Link href="#link">About Me</Nav.Link>
-                  <Nav.Link href="#link">Services</Nav.Link>
-                  <Nav.Link href="#link">Portfolio</Nav.Link>
-                  <NavDropdown title="Blog" id="blog-drop-down">
-                    <NavDropdown.Item href="#action/3.1">
-                      Blog
-                    </NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">
-                      Blog detail
-                    </NavDropdown.Item>
-                  </NavDropdown>
-                  <Nav.Link href="#link">Contact</Nav.Link>
-                  <Nav.Link href="#link">Hire Me</Nav.Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Stack>
+            </div>
+            <div className={'side-bar ' + styles.wrapSideBar }>
+              <div className={'side-bar ' + styles.sideBar }>
+                <Stack direction="horizontal" gap={3} className='justify-content-end'>
+                  <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="justify-content-end w-100">
+                      <Nav.Link href="#home">Home</Nav.Link>
+                      <Nav.Link href="#link">About Me</Nav.Link>
+                      <Nav.Link href="#link">Services</Nav.Link>
+                      <Nav.Link href="#link">Portfolio</Nav.Link>
+                      <NavDropdown title="Blog" id="blog-drop-down">
+                        <NavDropdown.Item href="#action/3.1">
+                          Blog
+                        </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">
+                          Blog detail
+                        </NavDropdown.Item>
+                      </NavDropdown>
+                      <Nav.Link href="#link">Contact</Nav.Link>
+                      <Nav.Link href="#link">Hire Me</Nav.Link>
+                    </Nav>
+                  </Navbar.Collapse>
+                </Stack>
+              </div>
+            </div>
           </Col>
         </Row>
       </Container>
