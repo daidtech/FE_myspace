57182abc4c0058b8f9cb134c09f8a68e499cc350
daidv <dvdai63@gmail.com>
2024-01-31 07:32:10 +0700

    update about page

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,38 +1,38 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import styles from './About.module.scss';
+import AboutSkill from './AboutSkill/AboutSkill';
 
 
 export default function About() {
   return (
-    <section className={styles.about}>
-      <Container>
-        <Row>
-          <Col lg={5}>
-            <div className={styles.aboutImageBox}>
-              <img src="/images/1.jpg" alt="" />
-            </div>
-          </Col>
-          <Col lg={7}>
-            <div className="content_box">
-              <h1>Why You Hire Us?</h1>
-              <p>Proin ipsum arcu, lobortis id ullamcorper nec, vehicula eget magna. Sed ut malesuada magna, eget lacinia sapien. Nam molestie non ex sit amet ornare. Nunc in pellentesque ipsum, vel vulputate sapien.</p>
-              <p>Suspendisse potenti. Aliquam elementum felis purus, quis vulputate libero semper nec. Morbi tincidunt maximus nisl vel placerat. Curabitur at ligula ac diam elementum hendrerit. Suspendisse nec metus eu est laoreet hendrerit vitae non ipsum. Nam faucibus elementum arcu, eget eleifend tortor.</p>
-              <p>Suspendisse potenti. Aliquam elementum felis purus, quis vulputate libero semper nec. Morbi tincidunt maximus nisl vel placerat. Curabitur at ligula ac diam elementum hendrerit. Suspendisse nec metus eu est laoreet hendrerit vitae non ipsum. Nam faucibus elementum arcu, eget eleifend tortor.</p>
-              <p>Suspendisse potenti. Aliquam elementum felis purus, quis vulputate libero semper nec. Morbi tincidunt maximus nisl vel placerat. Curabitur at ligula ac diam elementum hendrerit. Suspendisse nec metus eu est laoreet hendrerit vitae non ipsum. Nam faucibus elementum arcu, eget eleifend tortor.</p>
-              <p>Suspendisse potenti. Aliquam elementum felis purus, quis vulputate libero semper nec. Morbi tincidunt maximus nisl vel placerat. Curabitur at ligula ac diam elementum hendrerit. Suspendisse nec metus eu est laoreet hendrerit vitae non ipsum. Nam faucibus elementum arcu, eget eleifend tortor.</p>
-            </div>
-            <div className={styles.awardBox}>
-              <div className="award-image">
-                <img src="/images/award.png" alt="Award" />
+    <section className={styles.wrapAbout}>
+      <section className={styles.about}>
+        <Container>
+          <Row>
+            <Col lg={5}>
+              <div className={styles.aboutImageBox}>
+                <img src="/images/1.jpg" alt="" />
               </div>
-              <div className="award-description">
-                <h4 className="text-primary">Title award</h4>
-                <h4>description</h4>
+            </Col>
+            <Col lg={7}>
+              <div className="content_box">
+                <h1>Hi I'm Damas</h1>
+                <p>Proin ipsum arcu, lobortis id ullamcorper nec, vehicula eget magna. Sed ut malesuada magna, eget lacinia sapien. Nam molestie non ex sit amet ornare. Nunc in pellentesque ipsum, vel vulputate sapien.</p>
+                <p>Suspendisse potenti. Aliquam elementum felis purus, quis vulputate libero semper nec. Morbi tincidunt maximus nisl vel placerat. Curabitur at ligula ac diam elementum hendrerit. Suspendisse nec metus eu est laoreet hendrerit vitae non ipsum. Nam faucibus elementum arcu, eget eleifend tortor.</p>
+                <p>Suspendisse potenti. Aliquam elementum felis purus, quis vulputate libero semper nec. Morbi tincidunt maximus nisl vel placerat. Curabitur at ligula ac diam elementum hendrerit. Suspendisse nec metus eu est laoreet hendrerit vitae non ipsum. Nam faucibus elementum arcu, eget eleifend tortor.</p>
               </div>
-            </div>
-          </Col>
-        </Row>
-      </Container>
+              <div className={styles.awardBox}>
+                <div className="award-image">
+                  <img src="/images/award.png" alt="Award" />
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <img src="/images/1.jpg" alt="" className={styles.aboutBackground} />
+      <div className="bg-primary w-100 py-5"></div>
+      <AboutSkill />
     </section>
         )
 }
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,11 +1,9 @@
 import About from 'components/About/About';
-import AboutSkill from 'components/About/AboutSkill/AboutSkill';
 
 export default function index() {
   return (
    <>
     <About />
-    <AboutSkill />
    </>
   )
 }
