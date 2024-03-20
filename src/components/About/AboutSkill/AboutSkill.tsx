a71b4604a1b24d520d33fed5fc5c3940d91bfb36
daidv <dvdai63@gmail.com>
2024-03-20 07:25:54 +0700

    update skill block

--- src/components/About/AboutSkill/AboutSkill.tsx
+++ src/components/About/AboutSkill/AboutSkill.tsx
@@ -2,6 +2,19 @@ import { Container, Row, Col } from "react-bootstrap";
 import styles from './AboutSkill.module.scss';
 import RowTitle from "src/components/share/RowTitle/RowTitle";
 
+function BlockSkill({ title, skills }: Readonly<{title: string, skills: string[]}>) {
+  return (
+    <>
+      <h2 className={styles.skillBoxTitle}>{title}</h2>
+      <ul>
+        {skills.map((skill, index) => {
+          return (<li key={index}>{skill}</li>)
+        })}
+      </ul>
+    </>
+  )
+}
+
 export default function AboutSkill() {
 
   return (
@@ -11,50 +24,57 @@ export default function AboutSkill() {
         <Row>
           <Col lg={4}>
             <div className={styles.skillBox}>
-              <h2 className={styles.skillBoxTitle}>Programming languages</h2>
-              <ul>
-                <li> C#. NET </li>
-                <li> JavaScript </li>
-                <li> HTML, CSS </li>
-                <li> Java </li>
-              </ul>
-              <h2 className={styles.skillBoxTitle}>Frameworks & platforms</h2>
-              <ul>
-                <li>ReactJS / AngularJS / NodeJS</li>
-                <li>jQuery / Twitter Bootstrap / Materialize CSS</li>
-              </ul>
+              <BlockSkill
+                title="Programming languages"
+                skills={[
+                  "Ruby", "HTML/CSS", "JavaScript"
+                ]}
+              />
+              <BlockSkill
+                title="Frameworks & platforms"
+                skills={[
+                  "Ruby on Rails",
+                  "Reactjs, Vuejs",
+                  "jQuery / Twitter Bootstrap"
+                ]}
+              />
             </div>
           </Col>
           <Col lg={4}>
             <div className={styles.skillBox}>
-              <h2 className={styles.skillBoxTitle}>Programming languages</h2>
-              <ul>
-                <li> C#. NET </li>
-                <li> JavaScript </li>
-                <li> HTML, CSS </li>
-                <li> Java </li>
-              </ul>
-              <h2 className={styles.skillBoxTitle}>Frameworks & platforms</h2>
-              <ul>
-                <li>ReactJS / AngularJS / NodeJS</li>
-                <li>jQuery / Twitter Bootstrap / Materialize CSS</li>
-              </ul>
+
+              <BlockSkill
+                title="Databases"
+                skills={[
+                  "PostgreSQL"
+                ]}
+              />
+              <BlockSkill
+                title="Version Control/Deployment"
+                skills={[
+                  "Git", "GitHub", "AWS"
+                ]}
+              />
+              <BlockSkill
+                title="FOREIGN LANGUAGE"
+                skills={[
+                  "IELTS: 5.5"
+                ]}
+              />
             </div>
           </Col>
           <Col lg={4}>
             <div className={styles.skillBox}>
-              <h2 className={styles.skillBoxTitle}>Programming languages</h2>
-              <ul>
-                <li> C#. NET </li>
-                <li> JavaScript </li>
-                <li> HTML, CSS </li>
-                <li> Java </li>
-              </ul>
-              <h2 className={styles.skillBoxTitle}>Frameworks & platforms</h2>
-              <ul>
-                <li>ReactJS / AngularJS / NodeJS</li>
-                <li>jQuery / Twitter Bootstrap / Materialize CSS</li>
-              </ul>
+
+              <BlockSkill
+                title="KNOWLEDGE"
+                skills={[
+                  "Deep understanding about OOP, Design Pattern, SOLID principles, Dependency Injection, ...",
+                  "Good understanding about the Agile and Scrum process",
+                  "Good understanding software design, database design, RestfulAPI, ...",
+                  "Good time management, presentation and teamwork skills ...",
+                ]}
+              />
             </div>
           </Col>
         </Row>
